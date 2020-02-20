ac117ec8974e0f302981415772917831ff3d1264
Raul <raulgf0293@gmail.com>
2020-02-20 14:15:48 -0500

    Initial config for nuxt

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Hola Nuxt</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, asperiores magni? A similique exercitationem quos, doloribus eligendi perspiciatis consectetur eum, doloremque sed quaerat, ea quidem possimus nihil veniam vero commodi!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Nuxt",
+      meta: [
+        {
+          hid: "description",
+          name: "description"
+        }
+      ]
+    };
+  }
+};
+</script>
---
+++ pages/posts/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Posts</h1>
+    <ul v-for="post in posts" v-bind:key="post.id">
+      <Post :title="post.title" :id="post.id"></Post>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Post from "../../components/Post";
+export default {
+  data() {
+    return {
+      posts: []
+    };
+  },
+  components: {
+    Post
+  },
+  async created() {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    console.log(res);
+    this.posts = res.data;
+  }
+};
+</script>
